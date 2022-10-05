9792cb51e28aecc9a9d3a2caaf955f342903feb4
PatrickFaleiro <patrick_faleiro@hotmail.com>
2022-10-05 15:37:39 -0300

    Remoção do annotation @tags

---
+++ src/test/java/testRunner.java
@@ -0,0 +1,13 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(monochrome = true,
+        plugin = {"pretty", "html:target/cucumber"},
+        features = "src/test/features/",
+        dryRun = false)
+
+public class testRunner {
+
+}
